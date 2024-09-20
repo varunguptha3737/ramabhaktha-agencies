62f78a1b3908121d15d02340ee09044c98405337
varunguptha3737 <72660698+varunguptha3737@users.noreply.github.com>
2024-09-20 12:05:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Alert when subscribing to the newsletter
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the form from submitting
+    alert("Thank you for subscribing to our newsletter!");
+});
